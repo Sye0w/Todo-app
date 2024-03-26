5927da01342330ef84649dc863c88310baf687b6
Sye-5pace <owusilasyp00@gmail.com>
2024-03-26 18:51:04 +0000

    :boom: Style/Chore: update dependencies and ui

--- src/app/store/todo/todo.reducer.ts
+++ src/app/store/todo/todo.reducer.ts
@@ -1,44 +1,45 @@
 import { createReducer,on } from '@ngrx/store'
 import { addTodo,clearCompleted,updateTodo,reorderTodo  } from './todo.actions'
 
- export interface Todo {
-  id: number,
-  text: string;
-  active: boolean;
-  completed: boolean
-}
-
- export interface TodoState {
-  todos: Todo[]
+  export interface Todo {
+   id: number,
+   text: string;
+   active: boolean;
+   completed: boolean
  }
 
- const initialState: TodoState = {
-  todos: [],
- }
+  export interface TodoState {
+   todos: Todo[]
+  }
+
+  const initialState: TodoState = {
+   todos: [],
+  }
 
   export const todosReducer = createReducer(
-    initialState,
-    on(addTodo, (state, { text: string; active: boolean; completed: boolean }) => ({
-      ...state,
-      todos: [
-        ...state.todos,
-        { id: Date.now(), text, active, completed }
-      ]
-    })),
-    on(updateTodo, (state,payload) => ({
-      ...state,
-      todos: state.todos.map(todo =>
-        todo.id === payload.id ?
-        { ...todo, active: payload.active, completed: payload.completed }
+  initialState,
+  on(addTodo, (state, { text, active = true, completed = false }) => ({
+    ...state,
+    todos: [
+      ...state.todos,
+      { id: Date.now(), text, active, completed },
+    ],
+  })),
+  on(updateTodo, (state, payload) => ({
+    ...state,
+    todos: state.todos.map((todo: Todo) =>
+      todo.id === payload.id
+        ? { ...todo, active: payload.active, completed: payload.completed }
         : todo
-      )
-    })),
-    on(clearCompleted, (state) => ({
-      ...state,
-      todos: state.todos.filter((todo: Todo) => !todo.completed)
-    })),
-    on(reorderTodo, (state, { todos }: { todos: Todo[]) => ({
-      ...state,
-      todos
-    }))
- )
+    ),
+  })),
+  on(clearCompleted, (state) => ({
+    ...state,
+    todos: state.todos.filter((todo: Todo) => !todo.completed),
+  })),
+  on(reorderTodo, (state, { todos }) => ({
+    ...state,
+    todos,
+  })),
+);
+

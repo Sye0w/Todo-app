42f6c9a3061b4f09545e4cbe15ed97d9ecc0c599
owusilasyp00 <owusilasyp00@gmail.com>
2025-05-24 22:18:29 +0000

    🧱 Chore: add firebase setup to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,10 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { StoreModule } from '@ngrx/store';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { reducers } from './store';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +21,7 @@ export const appConfig: ApplicationConfig = {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000',
       })
-    ),
+    ), 
+    provideFirebaseApp(() => initializeApp({"projectId":"todo-app-5d980","appId":"1:800638133891:web:41306b1efde91f94619db6","storageBucket":"todo-app-5d980.firebasestorage.app","apiKey":"AIzaSyASlg7MdbX9TBDeHWmcpXSzBrFvFvaEPoU","authDomain":"todo-app-5d980.firebaseapp.com","messagingSenderId":"800638133891"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), providePerformance(() => getPerformance()),
   ],
 };
